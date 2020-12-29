373edb23b63b5900764d52901e3f68e9d112f69c
martinchristians <martinchristiansolihin@gmailcom>
2020-12-29 14:17:32 +0100

    Create Folder for all related Level Generator script and all related Player script

---
+++ Assets/Scripts/Player/PlayerController_Buttons.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// This script is applied to all buttons that is responsible for character movement via buttons in close play mode
+/// 
+/// @author : Martin Christian Solihin
+/// 
+/// </summary>
+
+public class PlayerController_Buttons : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private GameObject player;
+    private Rigidbody rb;
+    private bool isUpClicked, isLeftClicked, isRightClicked, isDownClicked;
+
+    public void Start() {
+        player = GameObject.Find("Player(Clone)");
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    // For moving forward and backward, fixed update will be used due to physic engine
+    private void FixedUpdate() {
+        if (isUpClicked) {
+            Debug.Log("UP");
+        } else if (isDownClicked) {
+            Debug.Log("DOWN");
+        }
+    }
+
+    // For rigt and left rotation, update method is enough
+    private void Update() {
+        if (isRightClicked) {
+            Debug.Log("RIGHT");
+        } else if (isLeftClicked) {
+            Debug.Log("LEFT");
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if(gameObject.name == "Up") {
+            isUpClicked = true;
+        } else if(gameObject.name == "Down") {
+            isDownClicked = true;
+        } else if(gameObject.name == "Right") {
+            isRightClicked = true;
+        } else if(gameObject.name == "Left") {
+            isLeftClicked = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        isUpClicked = false;
+        isDownClicked = false; 
+        isRightClicked = false;
+        isLeftClicked = false;
+    }
+}
